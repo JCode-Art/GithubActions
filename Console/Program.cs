b0f7a0de6859cfbd1e8aac7202ebf0e87c88efba
Julia Clausen <jclausen7@huskers.unl.edu>
2026-03-03 12:41:09 -0600

    Implement Power method and setup CI

--- Console/Program.cs
+++ Console/Program.cs
@@ -86,6 +86,8 @@ public class Program
 	// Implement this method following a similar pattern as above
 	public static double Power(string x, string y)
 	{
-		return 0.0;
+		  double baseNum = double.Parse(x);
+  	  double exponent = double.Parse(y);
+    	return Math.Pow(baseNum, exponent);
 	}
 }
